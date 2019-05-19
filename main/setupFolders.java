533364cb98686ca817b3206a475e593cc33e56b4
Mayr0n <50420555+Mayr0n@users.noreply.github.com>
2019-05-19 13:16:45 +0200

    Create setupFolders.java

---
+++ main/setupFolders.java
@@ -0,0 +1,28 @@
+package main;
+
+import net.dv8tion.jda.core.entities.Guild;
+
+import java.io.File;
+
+public class setupFolders {
+
+    public void setupFolders(Guild server){
+        File folderGen = new File("data/servers/");
+        File[] folders = folderGen.listFiles();
+
+        for(File folder : folders){
+            if(folder.getName().contains(server.getId())){
+                folder.renameTo(new File("data/servers/" + server.getId() +  " (" + server.getName() + ")/"));
+            }
+        }
+
+        File gen = new File("data/servers/" + server.getId() + " (" + server.getName() + ")/");
+        speedy.testFolderExist(gen);
+        speedy.testFolderExist(new File(gen.getPath() + "/moderation/"));
+        speedy.testFolderExist(new File(gen.getPath() + "/games/"));
+        speedy.testFolderExist(new File(gen.getPath() + "/games/pp"));
+        speedy.testFolderExist(new File(gen.getPath() + "/games/mathsplay"));
+        speedy.testFolderExist(new File(gen.getPath() + "/leaderboards/"));
+        speedy.testFolderExist(new File(gen.getPath() + "/leaderboards/pp"));
+    }
+}
