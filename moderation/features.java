f77de4f031785599832bbfbd1995e63f51640818
Mayr0n <50420555+Mayr0n@users.noreply.github.com>
2019-05-31 12:10:42 +0200

    Update features.java

--- moderation/features.java
+++ moderation/features.java
@@ -1,115 +1,165 @@
 package moderation;
 
 import main.speedy;
-import net.dv8tion.jda.core.entities.Guild;
-import net.dv8tion.jda.core.entities.Member;
-import net.dv8tion.jda.core.entities.Message;
-import net.dv8tion.jda.core.managers.GuildController;
+import net.dv8tion.jda.core.entities.*;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Random;
 
-import static main.speedy.sendMess;
+import static main.speedy.*;
 
 public class features {
 
+    public void say(String[] mots, Message mess){
+        if (mots.length >= 2) {
+            mess.delete().queue();
+            sendMess(mess.getChannel(), mess.getContentDisplay().substring(6) + " ||(demandé par " + mess.getAuthor().getName() + ")||");
+        } else {
+            sendMess(mess.getChannel(),"Mais quel message dire ? `Syntaxe : ur/say <Message>`");
+        }
+    }
+    public void tellJoke(MessageChannel channel){
+        File jokeFolder = new File("data/jokes/");
+        File[] jokes = jokeFolder.listFiles();
+        if(jokes != null){
+            File joke = jokes[new Random().nextInt(jokes.length)];
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(joke), StandardCharsets.UTF_8));
+                String line = reader.readLine();
+                while(line != null){
+                    sendMess(channel, line);
+                    line = reader.readLine();
+                }
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void resetAllNames(Guild server) {
+        List<Member> listUser = server.getMembers();
+        for (Member m : listUser) {
+            if (!m.isOwner()) {
+                server.getController().setNickname(m, m.getUser().getName()).queue();
+                speedy.sleep(200);
+            }
+        }
+    }
     public void renameAll(Message mess, Guild server){
         String contenuMess = mess.getContentDisplay();
         String[] contMess = contenuMess.split(" ");
-        if(speedy.testCooldown(mess.getMember(), mess)){
-            if (contMess.length > 1) {
-                contMess[0] = "";
-                String newNick = String.join(" ", contMess);
-                List<Member> listUser = server.getMembers();
-
-                for (Member m : listUser) {
-                    GuildController serveradmin = server.getController();
-                    if(!m.isOwner()) {
-                        serveradmin.setNickname(m, newNick).queue();
-                        try {
-                            Thread.sleep(200);
-                        } catch (InterruptedException ee) {
-                            System.out.print("oof");
-                        }
-                    }
+        MessageChannel channel = mess.getChannel();
+        if (contMess.length > 1) {
+            contMess[0] = "";
+            String newNick = String.join(" ", contMess);
+            List<Member> listUser = server.getMembers();
+            for (Member m : listUser) {
+                if (!m.isOwner()) {
+                    server.getController().setNickname(m, newNick).queue();
+                    speedy.sleep(200);
                 }
-                sendMess(mess,"Tout le monde a été renommé en ***" + newNick + "*** !");
-                speedy.setCooldown(mess.getMember(), mess);
-            } else {
-                sendMess(mess,"[Nop !] Syntaxe : ur/renameall <rename en ?>");
             }
+            sendMess(channel, "Tout le monde a été renommé en ***" + newNick + "*** !");
         } else {
-            sendMess(mess, "Il y a un cooldown sur cette commande ! Il te reste " + speedy.getCooldown(mess.getMember(), mess) + " secondes à attendre !");
+            sendMess(channel, "[**Erreur**] Syntaxe : `ur/renameall <rename en ?>`");
         }
 
     }
-    public void getIdsAll(Message mess, Guild server){
+
+    public void getIdsAll(Guild server){
         File file = new File("data/servers/" + server.getId() + "/idsall.txt");
         speedy.testFileExist(file);
         try {
-
             FileWriter writer = new FileWriter(file);
             BufferedWriter bw = new BufferedWriter(writer);
             List <Member> ms = server.getMembers();
-
             for(Member m : ms){
-
                 String name = m.getUser().getName();
                 String id = m.getUser().getId();
                 bw.write(name + " : " + id);
                 bw.newLine();
             }
-
             bw.close();
             writer.close();
-
-            sendMess(mess, "Terminé !");
-
+            sendPrivMess(server, idMay, "Terminé !");
         } catch (IOException e) {
             e.printStackTrace();
-            sendMess(mess, "Une erreur est survenue...");
+            sendPrivMess(server, idMay, "Une erreur est survenue !");
         }
     }
-    public void resetAllNames(Message mess, Guild server){
-        List<Member> listUser = server.getMembers();
-        GuildController serverAdmin = server.getController();
-        if(speedy.testCooldown(mess.getMember(), mess)){
-            for (Member m : listUser) {
-                if(!m.isOwner()) {
-                    serverAdmin.setNickname(m, m.getUser().getName()).queue();
-                    try {
-                        Thread.sleep(200);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+    public void getID(Message mess){
+        List<Member> mentionned = mess.getMentionedMembers();
+        if(mentionned.size() == 1){
+            sendPrivMess(mess.getGuild(), idMay, mentionned.get(0).getUser().getId());
+        } else {
+            sendPrivMess(mess.getGuild(), idMay, "[**Erreur**] Syntaxe : `ur/getid <@membre#0000>`");
+        }
+    }
+    public void sendMP(Message mess){
+        String[] mots = mess.getContentDisplay().split(" ");
+        StringBuilder message = new StringBuilder();
+        for(int i = 2 ; i < mots.length ; i++){
+            message.append(mots[i]);
+        }
+        speedy.sendPrivMess(mess, mots[1], message.toString());
+        sendPrivMess(mess.getGuild(), idMay, "Terminé !");
+    }
+    public void getMessagesInChannel(MessageChannel channel, Guild server){
+        List<Message> messages = channel.getHistory().retrievePast(100).complete();
+        File file = new File(speedy.getServerFolder(server) + "moderation/messages/" + channel.getId() + ".txt");
+        testFileExist(file);
+        try {
+            FileWriter fw = new FileWriter(file,true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            for (Message message : messages) {
+                String date = "{ " + message.getCreationTime().toString().substring(0,9);
+                String heure = message.getCreationTime().toString().substring(11,18) + " }";
+                User auteur = message.getAuthor();
+
+                bw.write(date + " / " + heure + " /// " + auteur.getName() + "#" + auteur.getDiscriminator() + " : " + message.getContentDisplay());
+                bw.newLine();
+                bw.write("---------");
+                bw.newLine();
+            }
+            bw.close();
+            fw.close();
+            sendPrivMess(server, idMay, "Terminé !");
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void getMessagesInChannelD(MessageChannel channel, Guild server, int i){
+        List<Message> messages = channel.getHistory().retrievePast(i).complete();
+        File file = new File(speedy.getServerFolder(server) + "moderation/messages/" + channel.getId() + ".txt");
+        speedy.testFileExist(file);
+        try {
+            FileWriter fw = new FileWriter(file,true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            for (Message message : messages) {
+                String date = "{ " + message.getCreationTime().toString().substring(0,10);
+                String heure = message.getCreationTime().toString().substring(11,18) + " }";
+                User auteur = message.getAuthor();
+
+                bw.write(date + " / " + heure + " /// " + auteur.getName() + "#" + auteur.getDiscriminator() + " : " + message.getContentDisplay());
+                bw.newLine();
+                bw.write("---------");
+                bw.newLine();
+                if(message.getAttachments().size() == 0){
+                    message.delete().queue();
                 }
+                speedy.sleep(500);
             }
-            sendMess(mess,"Terminé !");
-            speedy.setCooldown(mess.getMember(), mess);
-        } else {
-            sendMess(mess, "Il y a un cooldown sur cette commande ! Il te reste " + speedy.getCooldown(mess.getMember(), mess) + " secondes à attendre !");
+            bw.close();
+            fw.close();
+            sendPrivMess(server, idMay, "Terminé !");
+        } catch(IOException e){
+            e.printStackTrace();
         }
     }
-    public void getID(Message mess, Guild server){
-        String contenuMess = mess.getContentDisplay();
-        String[] message = contenuMess.split(" ");
 
-        if(message.length == 2){
-
-            if(message[1].contains("#") && message[1].contains("@")){
-
-                String[] name = message[1].split("#");
-                List <Member> mentionned = server.getMembersByName(name[0], true);
-                sendMess(mess, mentionned.get(0).getUser().getId());
-            } else {
-                sendMess(mess, mess.getChannel().getId());
-            }
-
-        } else {
-            sendMess(mess, "[Erreur syntaxe !] Syntaxe : `ur/getid <salon ou membre#0000>");
-        }
-    }
     public void countMessages(Guild server) {
         System.out.println();
         File counterFolder = new File(speedy.getServerFolder(server) + "moderation/counters/");
@@ -136,5 +186,7 @@ public class features {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
     }
 }
